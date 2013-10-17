d352709209a77498e264ae7f5a11a934c1a8bd93
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-10-17 02:42:16 -0400

    edited the web.rb file

--- web.rb
+++ web.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  "This is the website for America Solidaria. It is being completed as a project for Introduction to E-Technology CSE40613 at the university of Notre Dame"
 end
