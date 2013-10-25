574ada24bbff18b5d21614980f23004105f74030
siruilu <slu@nd.edu>
2013-10-25 00:17:53 -0400

    Testing

--- web.rb
+++ web.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "This is the website for America Solidaria. It is being completed as a project for Introduction to E-Technology CSE40613 at the university of Notre Dame"
+  "This is the website for America Solidaria. It is being completed as a project for Introduction to E-Technology CSE40613 at the University of Notre Dame"
+  "Testing"
+
 end
